69286401724635cf0aa5a61c15552a52ae5727de
Desire32 <58604764+Desire32@users.noreply.github.com>
2023-12-15 14:39:00 +0200

    Some little changes

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -38,18 +38,16 @@ void main()
 	IMesh* islandMesh = myEngine->LoadMesh("island.x");
 	IModel* island = islandMesh->CreateModel(0, -5, 0);
 
-	IMesh* sphereMesh = myEngine->LoadMesh("sphere.x");
+	IMesh* sphereMesh = myEngine->LoadMesh("spheremesh.x");
 	IModel* sphere = sphereMesh->CreateModel(0, 10, 0);
 
 	IMesh* cubeMesh = myEngine->LoadMesh("minicube.x");
-	
 
 	IMesh* skyboxMesh = myEngine->LoadMesh("skybox.x");
 	IModel* skybox = skyboxMesh->CreateModel(0, -960, 0);
 
 	ICamera* camera = myEngine->CreateCamera(kManual,0,200,0);
 	camera->RotateLocalX(90);
-
 
 	IModel* cubeArray[13];
 	for (int CubeAmount = 12; CubeAmount > 0; CubeAmount--) {
@@ -75,19 +73,19 @@ void main()
 			break;
 		}
 		if (myEngine->KeyHeld(Key_W)) {
-			sphere->MoveZ(kSphereSpeed);
+			sphere->MoveLocalZ(kSphereSpeed);
 		}
 		if (myEngine->KeyHeld(Key_S)) {
-			sphere->MoveZ(-kSphereSpeed);
+			sphere->MoveLocalZ(-kSphereSpeed);
 		}
 		if (myEngine->KeyHeld(Key_A)) {
-			sphere->MoveX(-kSphereSpeed);
+			sphere->RotateLocalY(-kSphereSpeed * 2);
 		}
 		if (myEngine->KeyHeld(Key_D)) {
-			sphere->MoveX(kSphereSpeed);
+			sphere->RotateLocalY(kSphereSpeed * 2);
 		}
 
-		if (myEngine->KeyHit(Key_P)) {
+		if (myEngine->KeyHit(Key_P) && !(GameState == Win)) {
 			if (PlayingState == Playing) {
 				kSphereSpeed = 0;
 				PlayingState = Stop;
@@ -111,7 +109,6 @@ void main()
 			camera->MoveX(kSphereSpeed);
 		}
 
-
 		for (int CubeAmount = 12; CubeAmount > 0; CubeAmount--) {
 
 			float cubeX = cubeArray[CubeAmount]->GetX() - sphere->GetX();
@@ -124,10 +121,22 @@ void main()
 				playerPoints += 10;
 			}
 		}
-	
+
 		stringstream outText;
-		outText << "Points: " << playerPoints;
+		if (GameState == inProcess) {
+		outText << "Points:" << playerPoints;
 		myFont->Draw(outText.str(), fontX, fontY, kBlack);
+		outText.str("");
+
+		if (playerPoints >= 50) {
+			outText << "You Won!";
+			fontX = 500.0f;
+			fontY = 335.0f;
+			myFont->Draw(outText.str(), fontX, fontY, kRed);
+			kSphereSpeed = 0;
+		}
+	}
+
 		if (playerPoints >= Upgrade) {
 			sphere->Scale(1.2f);
 			Upgrade += Upgrade;
@@ -135,33 +144,30 @@ void main()
 			sphere->MoveY(5);
 		}
 
-		if (playerPoints >= 30 && GameState == inProcess) {
-			outText << "You Won!";
-			fontX = 30.0f;
-			fontY = 30.0f;
-			myFont->Draw(outText.str(), fontX, fontY, kBlack);
-		}
-		
+		/*
+		if (sphere->GetX() > island->GetLocalX()) {
+			outText << "You LOSE!";
+			fontX = 500.0f;
+			fontY = 330.0f;
+			myFont->Draw(outText.str(), fontX, fontY, kRed);
+			kSphereSpeed = 0;
+		}*/
 
 		if (myEngine->KeyHit(Key_2)) {
 			if (CameraState == NotPressed) {
 				ICamera* camera = myEngine->CreateCamera(kManual, 150, 150, -150);
 				camera->RotateLocalY(-45);
 				camera->RotateLocalX(45);
-
 				CameraState = Pressed;
 			}
-
 		}
 		if (myEngine->KeyHit(Key_1)) {
 			if (CameraState == Pressed) {
 				ICamera* camera = myEngine->CreateCamera(kManual, 0, 200, 0);
 					camera->RotateLocalX(90);
-
 					CameraState = NotPressed;
 			}
 		}
-
 	}
 	myEngine->Delete();
 }
